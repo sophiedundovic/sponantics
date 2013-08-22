b16b86e9750f4b320cfbab5e12339c61bd19be3e
Sophie Dundovic <sophie.dundovic@gmail.com>
2013-08-22 18:43:13 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sponantics::Application.config.secret_key_base = '0735d385c91d764381b0e99910d3c2316ad1a275ea615ae38c0bbd1137f52ec1cd5d74c34c36418eb365e77a53f779491dc2c30ad46e7c54e8d24e0d21744dbb'
